199e52410cb6fdf7091317476a9b24f0be4c367f
Nikolaj93 <nikolaj.hansen93@gmail.com>
2021-02-18 03:57:44 +0100

    initial commit

---
+++ Exercise1.c
@@ -0,0 +1,66 @@
+//
+// Created by Nikolaj on 18/02/2021.
+//
+#include <stdio.h>
+
+int main( )
+{
+    /*
+    // Exercise 1
+    int amount;
+    double price, totalPrice;
+    double vat, vatAmount, finalPrice;
+
+    printf("------------Exercise 1------------\n");
+
+    amount = 41112;
+    printf("amount %i stk\n", amount);
+
+    price = 11.48;
+    printf("price %.2lf kr\n", price);
+
+    totalPrice = amount * price;
+    printf("total %.2lf kr (without tax)\n", totalPrice);
+    printf("----------------------------------\n");
+
+    vat = 25;
+    printf("vat %.0lf %%\n", vat);
+
+    vatAmount = totalPrice * 0.25;
+    printf("tax amount is %.2lf kr\n", vatAmount);
+    printf("----------------------------------\n");
+
+    finalPrice = totalPrice * 1.25;
+    printf("final price with tax %.2lf kr\n", finalPrice);
+    printf("==================================\n");
+    */
+
+    /*
+    // Exercise 2
+    printf("\n");
+    printf("------------Exercise 2------------\n");
+    int number;
+    char character;
+    char string[20];
+    scanf("%s %c %d", &string, &character, &number);
+    printf("%s %c %d", string, character, number);
+    printf("==================================\n");
+    */
+
+    // Exercise 3
+    printf("\n");
+    printf("------------Exercise 3------------\n");
+    char character;
+    char name[20];
+    gets(name);
+    character = getchar();
+
+    printf("==================================\n");
+
+    // Exercise 4
+    printf("\n");
+    printf("------------Exercise 4------------\n");
+
+
+    return 0;
+}
